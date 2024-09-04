fd4e5bcf8437c3cf76c3e4db47831c1886c75800
shamlinishad <nsha1564@gmail.com>
2024-09-04 09:01:05 -0400

    Committing Service Registry application

---
+++ service.registry/src/test/java/com/sha/service/registry/ServiceRegistryLauncherTests.java
@@ -0,0 +1,13 @@
+package com.sha.service.registry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceRegistryLauncherTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
