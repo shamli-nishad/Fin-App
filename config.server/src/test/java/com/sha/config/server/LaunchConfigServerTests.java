74d9600a6455b51a3f02e07b0ddbd1d0e80d4031
shamli-nishad <shamli.n@outlook.com>
2024-09-11 23:40:18 -0400

    Initial commit for common config server

---
+++ config.server/src/test/java/com/sha/config/server/LaunchConfigServerTests.java
@@ -0,0 +1,13 @@
+package com.sha.config.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LaunchConfigServerTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
